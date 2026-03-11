31b6d7649644db3202a94507337b7e66c0007ada
Greyo0 <arthurrodriguesgrey@gmail.com>
2026-03-10 22:40:19 -0300

    implementado empresaA

---
+++ mandar_arquivo.py
@@ -0,0 +1,41 @@
+import socket
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+with open("keys/empresaBpublic.pem", "rb") as f:
+    public_key = RSA.import_key(f.read())
+
+aes_key = get_random_bytes(32)
+
+cipher_rsa = PKCS1_OAEP.new(public_key)
+aes_encriptado = cipher_rsa.encrypt(aes_key)
+
+with open("docs/bomdia.pdf", "rb") as file:
+    data = file.read()
+
+hash_obj = SHA256.new(data)
+hash_bytes = hash_obj.digest()
+
+mensagem = hash_bytes + data
+
+cipher = AES.new(aes_key, AES.MODE_GCM)
+ciphertext, tag = cipher.encrypt_and_digest(mensagem)
+
+payload = (
+    len(aes_encriptado).to_bytes(4, "big") +
+    aes_encriptado +
+    cipher.nonce +
+    tag +
+    ciphertext
+)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(payload)
+
+print("Arquivo enviado")
